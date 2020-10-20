e2df9bea097e01ee02d1052c286e16d241d839ea
Cyber-Empire <72108341+Cyber-Empire@users.noreply.github.com>
2020-10-20 22:54:01 +0200

    interactive forms
    my first interactive from, with HTML forms and php.

---
+++ firstform.php
@@ -0,0 +1,14 @@
+
+
+
+
+<?php
+
+echo  "the number you entered is :".$_GET['number'];
+
+?>
+<p>Enter the number ?!</p>
+<form>
+<input  name="number" type="text">
+<input type="submit" value = "Enter">
+</form>
