0c1675990e44af0266bd12991e1a4bcbfb2db6ce
Cyber-Empire <72108341+Cyber-Empire@users.noreply.github.com>
2020-11-13 12:59:39 +0100

    Update main.php
    all done until this point .

--- main.php
+++ main.php
@@ -3,20 +3,31 @@
    $error = "";
   if($_GET['url']){
   
-  $urlContents = file_get_contents("https://api.ssllabs.com/api/v3/analyze?host=".urlencode($_GET['url'])."&startNew=on&all=done");
+  $urlContents = file_get_contents("https://api.ssllabs.com/api/v3/analyze?host=".urlencode($_GET['url'])."&all=on");
      $UrlArray = json_decode($urlContents, true);    
-       print_r($UrlArray);
+      // print_r($UrlArray);
 	   if($UrlArray['status']== "READY" || "IN_PROGRESS")
 	   {
-	   $result = "Result for --".$_GET['url']." --is as follows :<br>"; 
+	   $result = "Result for --".$_GET['url']." -- is as follows :<br>"; 
 	   $CertificateVersion= $UrlArray['criteriaVersion'];
-	   $result.= "certificate version  ->".$CertificateVersion."<br>";
+	   $result.= "Certificate version  ->".$CertificateVersion."<br>";
 	   $ServerName = $UrlArray['endpoints'][0]['serverName'];
-	   $result.="Servername-> ".$ServerName."br";
+	   $result.="Servername-> ".$ServerName."<br>";
 	  $ipadd=$UrlArray['endpoints'][0]['ipAddress'];
 	  $result .= "Ip Address ->".$ipadd."<br>";
 	  $grade = $UrlArray['endpoints'][0]['grade'];
-	  $result .= "possible values: A+, A-, A-F, T (no trust) and M (certificate name mismatch) ->".$grade;
+	  $id = $UrlArray['endpoints'][0]['details']['certChains'][0]['id'];
+	  $result.="Cert id  ->".$id."<br>";
+	  $Ciphersuites = $UrlArray['endpoints'][0]['details']['suites'][0]['list'][0]['q'];
+	  $result.="Flag for suite insecure or weak. Not present if suite is strong or good (0 insecure,1 weak)->".$Ciphersuites."<br>";
+	  $result .= "Cert grade has possible values: A+, A-, A-F, T (no trust) and M (certificate name mismatch),your grade is ->".$grade."<br>";
+	  $openSsl = $UrlArray['endpoints'][0]['details']['openSslCcs'];
+	  $result .= "Result of the CVE-2014-0224 test(-1 test failed,0 unknown,1 not vulnerable,2 possibly vulnerable,3 vulnerable and exploitable) ->".$openSsl."<br>"; 
+	  $openSSLLucky = $UrlArray['endpoints'][0]['details']['openSSLLuckyMinus20'];
+	  $result .= " Result of the CVE-2016-2107 test(-1 test failed,0 unknown,1 not vulnerable,2 vulnerable and insecure) ->".$openSSLLucky."<br>";
+	  $ticketbleed = $UrlArray['endpoints'][0]['details']['ticketbleed'];
+	  $result .= "Result of the ticketbleed CVE-2016-9244 test(-1 test failed,0 unknown,1 not vulnerable,2 vulnerable and insecure,3 not vulnerable but a similar bug detected) ->".$ticketbleed."<br>";
+	  
 	   }
 	   else
 	   {
@@ -45,11 +56,17 @@
 	}
 	.container{
 		text-align : center;
-		margin-top:175px;
-		width:450px;
+		margin-top:100px;
+		
 	
 	}
-	
+	html{
+		background :url(https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) no-repeat center center fixed ;
+		--webkit-background-size:cover;
+		-moz-background-size:cover;
+		-o-background-size:cover;
+		background-size:cover;
+	}
 	
 	
 	<!--making the background image fix -->
@@ -57,6 +74,7 @@
 	
   </head>
   <body>
+  
    
    <div class = "container">
    <h1 style = "color :white"> What is  your URL ;) </h1>
