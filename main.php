c401dbd90a4fde3ae233941ba08605c81f3ce4b8
Cyber-Empire <72108341+Cyber-Empire@users.noreply.github.com>
2020-12-02 21:48:22 +0100

    Update main.php
    adding more tests
    adding status to the loading bar
    adding the total time that test took

--- main.php
+++ main.php
@@ -11,7 +11,7 @@ if(isset($_GET['submit']))
    {
 	   $urlContents = file_get_contents("https://api.ssllabs.com/api/v3/analyze?host=".$_GET['url']."&all=on");
        $UrlArray = json_decode($urlContents, true); 
-	  // print_r($UrlArray);
+	 // print_r($UrlArray);
 	   echo"<br>";
 	
   
@@ -21,7 +21,9 @@ if(isset($_GET['submit']))
 	    {
 		   echo "<font color =#42f545> All done ✔ </font>"."<br>";
 	   echo "Result for --"."<font color = #f56642>".$_GET['url']."</font>"." -- is as follows :<br>"; 
-	   
+	   $time = $UrlArray['endpoints']['0']['duration'];
+	   $time = number_format($time/60000,2);
+	   echo  "<font color=#14B8CD>Duration of Tests </font>".":".$time."<font color=#14B8CD>minutes</font>"."<br>";
 	   $CertificateVersion= $UrlArray['criteriaVersion'];
 	   echo "<font color=#14B8CD>Certificate version</font>".":".$CertificateVersion."<br>";
 	   
@@ -34,25 +36,135 @@ if(isset($_GET['submit']))
 	  $id = $UrlArray['endpoints'][0]['details']['certChains'][0]['id'];
 	  echo "<font color=#14B8CD>Certificate identification</font>".":".$id."<br>";
 	  $openSsl = $UrlArray['endpoints'][0]['details']['openSslCcs'];
-	  echo "<font color=#14B8CD>Certificate  CVE-2014-0224 test</font>".":".$openSsl."<br>";
+	  if($openSsl == 1)
+	  {
+		echo "<font color=#14B8CD>Certificate  CVE-2014-0224 test</font> : not vulnerable"."<br>";  
+	  }
+	  elseif($openSsl == -1)
+	  {
+		echo "<font color=#14B8CD>Certificate  CVE-2014-0224 test</font> : test failed (vulnerable)"."<br>";  
+	  }
+	   elseif($openSsl == 2)
+	  {
+		echo "<font color=#14B8CD>Certificate  CVE-2014-0224 test</font> : possibly vulnerable, but not exploitable"."<br>";  
+	  }
+	    elseif($openSsl == 3)
+	  {
+		echo "<font color=#14B8CD>Certificate  CVE-2014-0224 test</font> : vulnerable and exploitable"."<br>";  
+	  }
+	    else
+	  {
+		echo "<font color=#14B8CD>Certificate  CVE-2014-0224 test</font> : unkown"."<br>";  
+	  }
+	  
+	  
+	  
 	  $openSSLLucky = $UrlArray['endpoints'][0]['details']['openSSLLuckyMinus20'];
-	  echo "<font color=#14B8CD>Certificate CVE-2016-2107 test </font>".":".$openSSLLucky."<br>";
+	 
+	  if($openSSLLucky == 1)
+	  { 
+		echo "<font color=#14B8CD>Certificate  CVE-2016-2107 test</font> : not vulnerable"."<br>";  
+	  }
+	  elseif($openSSLLucky== -1)
+	  {
+		echo "<font color=#14B8CD>Certificate  CVE-2016-2107 test</font> : test failed (vulnerable)"."<br>";  
+	  }
+	   elseif($openSSLLucky== 2)
+	  {
+		echo "<font color=#14B8CD>Certificate  CVE-2016-2107 test</font> : possibly vulnerable, but not exploitable"."<br>";  
+	  }
+	   
+	   else
+	  {
+		echo "<font color=#14B8CD>Certificate  CVE-2016-2107 test</font> : unkown"."<br>";  
+	  }
+	  
+	  
 	  $ticketbleed = $UrlArray['endpoints'][0]['details']['ticketbleed'];
-	  echo "<font color=#14B8CD> Certificate ticketbleed CVE-2016-9244 test</font>".":".$ticketbleed."<br>";
-	echo"Test results can be"."<font color=#eb4034 >(-1 test failed),(0 unknown),(2 vulnerable and insecure)</font>";
-	    }
+	  
+	 if($ticketbleed == 1)
+	  {
+		echo "<font color=#14B8CD>Certificate ticketbleed CVE-2016-9244 test</font> : not vulnerable"."<br>";  
+	  }
+	  elseif($ticketbleed == -1)
+	  {
+		echo "<font color=#14B8CD>Certificate ticketbleed CVE-2016-9244 test</font> : test failed (vulnerable)"."<br>";  
+	  }
+	   elseif($ticketbleed == 2)
+	  {
+		echo "<font color=#14B8CD>Certificate ticketbleed CVE-2016-9244 test</font> : possibly vulnerable, but not exploitable"."<br>";  
+	  }
+	    elseif($ticketbleed == 3)
+	  {
+		echo "<font color=#14B8CD>Certificate ticketbleed CVE-2016-9244 test</font> : vulnerable and exploitable"."<br>";  
+	  }
+	    else
+	  {
+		echo "<font color=#14B8CD>Certificate ticketbleed CVE-2016-9244 test</font> : unkown"."<br>";  
+	  }
+	  
+	  
+	  $bleichenbacher = $UrlArray['endpoints'][0]['details']['bleichenbacher'];
+     if($bleichenbacher == 1)
+	  {
+		echo "<font color=#14B8CD> Results of the Return Of Bleichenbacher's Oracle Threat (ROBOT) test:</font> : not vulnerable"."<br>";  
+	  }
+	  elseif($bleichenbacher == -1)
+	  {
+		echo "<font color=#14B8CD> Results of the Return Of Bleichenbacher's Oracle Threat (ROBOT) test:</font> : test failed (vulnerable)"."<br>";  
+	  }
+	   elseif($bleichenbacher == 2)
+	  {
+		echo "<font color=#14B8CD> Results of the Return Of Bleichenbacher's Oracle Threat (ROBOT) test:</font> : vulnerable (weak oracle)"."<br>";  
+	  }
+	    elseif($bleichenbacher == 3)
+	  {
+		echo "<font color=#14B8CD> Results of the Return Of Bleichenbacher's Oracle Threat (ROBOT) test:</font> : vulnerable (strong oracle)"."<br>";  
+	  }
+	    elseif($bleichenbacher == 4)
+	  {
+		echo "<font color=#14B8CD> Results of the Return Of Bleichenbacher's Oracle Threat (ROBOT) test:</font> : inconsistent results"."<br>";  
+	  }
+	   else
+	  {
+		echo "<font color=#14B8CD>Certificate ticketbleed CVE-2016-9244 test</font> : unkown"."<br>";  
+	  }
+	  
+	  
+	  
+	  
+	  $dhUsesKnownPrimes= $UrlArray['endpoints'][0]['details']['dhUsesKnownPrimes'];
+	  if($dhUsesKnownPrimes == 0)
+	  {
+		echo "<font color=#14B8CD> whether the server uses known DH primes. Not present if the server doesn't support the DH key exchange:</font> : no"."<br>";  
+	  }
+	  elseif($dhUsesKnownPrimes == 1)
+	  {
+		echo "<font color=#14B8CD>whether the server uses known DH primes. Not present if the server doesn't support the DH key exchange</font>: yes, but they're not weak"."<br>";  
+	  }
+	   elseif($dhUsesKnownPrimes == 2)
+	  {
+		echo "<font color=#14B8CD>whether the server uses known DH primes. Not present if the server doesn't support the DH key exchange:</font> : yes, but they're  weak"."<br>";  
+	  }
+	   
+	
+	    
+		}
      
 	  
 	  elseif($UrlArray ['status']=="Unable to resolve domain name" || $UrlArray ['status']=="ERROR"  )
 	  {
 		  echo "Undefined URL!!";
 	  }
+	  
 	  else
 	    {
 	       echo "please wait ,loading on progress ,lt may take a while !"."<br>";
 		   $counter = 0;
 		   $counter += $UrlArray['endpoints']['0']['progress'];
-		   echo "Loading on progres [".$counter."%]loaded !"."<br>";	
+		   echo "Loading on progres [".$counter."%]loaded !"."<br>";
+           $testStatus = $UrlArray['endpoints']['0']['statusDetails'];
+		   echo "Description of the operation currently in progress  : ".$testStatus;
              header("refresh:10");
 			 // refreshing the page each 10 second
  
@@ -71,6 +183,7 @@ if(isset($_GET['submit']))
 <style>
 .main{
 	text-align:center;
+	margin-top:200px;
 }
 	
 </style>
@@ -82,7 +195,6 @@ if(isset($_GET['submit']))
     <input type="text" name = "url" placeholder = "www.something.com">
      <button type="submit" name="submit">Check</button>
     </form>
-	<img src ="https://media.giphy.com/media/lo5HLcAPFSgTZNTpAn/giphy.gif">
- </div>
+	 </div>
  </body>
 </html>
